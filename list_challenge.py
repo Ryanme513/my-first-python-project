4e660abde6c5eda62e72f6e6643adee538b3be45
Ryanme513 <Ryanyu512@gmail.com>
2020-10-19 21:59:44 -0700

    hi

--- list_challenge.py
+++ list_challenge.py
@@ -58,10 +58,50 @@
 #Uncomment the line below when your function is done
 #print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
 
-def more_than_n(lst, item, n):
-    if lst.count(item) > n:
-        return True
-    else:
-        return False
-print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+# def more_than_n(lst, item, n):
+#     if lst.count(item) > n:
+#         return True
+#     else:
+#         return False
+# print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
     
+#----------------------------------------------------------
+# Challenge 4 - Append Size
+#----------------------------------------------------------
+
+# Create a function called append_size that has one parameter named lst.
+
+# The function should append the size of lst (inclusive) to the end of lst. The function should then return this new list.
+
+# For example, if lst was [23, 42, 108], the function should return [23, 42, 108, 3] because the size of lst was originally 3.
+
+#Write your function here
+
+
+#Uncomment the line below when your function is done
+#print(append_size([23, 42, 108]))
+
+# def append_size(lst):
+#     lst.append(len(lst))
+#     return lst
+# print(append_size([23, 42, 108]))
+
+#----------------------------------------------------------
+# Challenge 5 - Combine Sort
+#----------------------------------------------------------
+
+# Write a function named combine_sort that has two parameters named lst1 and lst2.
+
+# The function should combine these two lists into one new list and sort the result. Return the new sorted list.
+
+#Write your function here
+
+
+#Uncomment the line below when your function is done
+#print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+def combine_sort(lst1, lst2):
+    x = list(zip(lst1, lst2))
+    x.sort()
+    return x
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
