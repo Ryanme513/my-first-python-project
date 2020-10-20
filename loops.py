ded600492cd607938ca1d1849ab64d137d1ad02e
Ryanme513 <Ryanyu512@gmail.com>
2020-10-19 18:52:04 -0700

    hi

--- loops.py
+++ loops.py
@@ -2,8 +2,8 @@
 # for name in names:
 #     print(name)
 
-rng = list(range(6))
-print(rng)
+# rng = list(range(6))
+# print(rng)
 
 # lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # for num in lst:
@@ -13,11 +13,43 @@ print(rng)
 #     lst[index] = 2 * lst[index]
 #     print(lst[index])
 
-lst = [1, 2, 3, 4, 5, 6, 7]
+# lst = [1, 2, 3, 4, 5, 6, 7]
+# for num in lst:
+#     print(num2)
+# print(lst)
+# for index in range(len(lst)):
+#     lst[index] = lst[index]
+#     print(lst[index])
+# print(lst)
+
+# names = ["alex", "allistair", "andrew", "angelo", "elijah", "george", "jonathan"]
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[4])
+# print(names[5])
+# print(names[6])
+names = ["alex", "allistair", "andrew", "angelo", "elijah", "george", "jonathan"]
+for person in names:
+    print(person)
+# person = "alex"
+# print(person)
+# person = "allistair"
+# print(person)
+# person = "andrew"
+# print(person)
+# rng = list(range(6))
+# print(rng)
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 for num in lst:
-    print(num2)
-print(lst)
-for index in range(len(lst)):
-    lst[index] = lst[index]
+    print(num*2)
+print("unmodified list: ", lst)
+#range(6) -> [0, 1, 2, 3, 4, 5]
+#range(8) -> [0, 1, 2, 3, 4, 5, 6, 7]
+#range(len(lst)) -> [0, 1, 2, 3, 4, 5, ... len(lst) - 1]
+list_length = len(lst)
+for index in range(list_length):
+    lst[index] = 2 * lst[index]
     print(lst[index])
-print(lst)
+print("modified list: ", lst)
